93835b2c231876a83948181293cb76fb5ec39141
adityapryg <aditya@brainmatics.com>
2024-03-27 11:53:57 +0700

    Belajar Unit Test

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_register_page_is_accessible(): void
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+        $response->assertSeeText("Register");
+    }
+
+    public function test_new_user_can_register()
+    {
+        //register user
+        $response = $this->post('/register', [
+            'name' => 'Aditya Prayoga',
+            'email' => 'aditya@brainmatics.com',
+            'password' => 'strongpassword',
+            'password_confirmation' => 'strongpassword'
+        ]);
+        //check user sudah ada di database
+        $this->assertTrue(User::whereEmail("aditya@brainmatics.com")->exists());
+        //berhasil authentikasi
+        $this->assertAuthenticated();  //berhasil logins
+        //postcondition diarahkan ke /home
+        $response->assertRedirect('/home');
+    }
+}
