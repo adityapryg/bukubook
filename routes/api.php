e49e47f8ad48f6fa4f3b3073f9b46d6f73312c0e
Aditya Prayoga <aditya@brainmatics.com>
2024-11-13 13:26:18 +0700

    add book api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\Book as BookResource;
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/books', function () {
+    return BookResource::collection(Book::paginate(10));
 });
